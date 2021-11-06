90b0fb9944c9c367ec83e20294c013868df130e2
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-06 18:27:45 +0000

    created the about routes

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/routes', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+app.get('/about', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/about.html'))
 );
 
 app.listen(PORT, () =>
