565a93f668ee808ceb30e2747e17bccd46551371
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-06 18:15:50 +0000

    added a route to index.html

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.static());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.get('/routes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
+
