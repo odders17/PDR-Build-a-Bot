f14114a9b9a9f465afaf7da4df28bc2b87dd249c
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-19 19:20:18 +0000

    server.js

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 const PORT = process.env.port || 3000;
-
+connectDB();
 //middleware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
-app.get('/about', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/about.html'))
+app.get("/about", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/about.html"))
 );
-
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
-
-
-
