8a7ae2a03533775abbdb461f1fc6085f8dbedda2
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-22 16:34:27 +0000

    completed the product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
-//@desc GET a product by id from db
+const { getAllProducts, getProductById} = require("../controller/productControllers")
+
+
+//@desc GET all products from db
+//@route GET/api/products
+//@access Public
+router.get("/", getAllProducts);
+
+//@desc GET all products from db
 //@route GET/api/products/:id
 //@access Public
-//@
-router.get("/:id", (req, res) => {
-
-
-})
+router.get("/:id", getProductById);
 
 module.exports = router;
+
+
