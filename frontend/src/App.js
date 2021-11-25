a7145b13cebbca04a780e6f0abbf0daadbee82a5
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-25 23:48:45 +0000

    clenaed up the front-end by deleting unneeded files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
 import Home from "./pages/Home";
 import Menu from "./components/Navbar";
+// going to need a Navbar
+// going to need a Sidedraw
+// going to need a backdrop
+// going to need to have a homescreen 
+// going to need a product screen
+// going to need a cartscreen
+
 
 function App() {
   return (
