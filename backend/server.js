0f055087a2b930e1cb904202dba7891a2871940a
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-25 19:50:40 +0000

    react frontend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config("../");
 const express = require("express");
 // const path = require("path");
 const connectDB = require("./config/db");
-const productRoutes = require("./routes/productRoutes")
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 const PORT = process.env.port || 3000;
@@ -15,15 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/products", productRoutes);
 
-
-
-
-
 // app.get("/", (req, res) =>
 //   res.sendFile(path.join(__dirname, "public/index.html"))
 // );
 // app.get("/about", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/about.html"))
+//   res.send  File(path.join(__dirname, "public/about.html"))
 // );
 
 app.listen(PORT, () =>
