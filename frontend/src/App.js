673f0cf942d765650fd1faf87e31c329aa208d77
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-11-26 11:40:16 +0000

    gql

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import Menu from "./components/Navbar";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 // going to need a Navbar
 // going to need a Sidedraw
 // going to need a backdrop
@@ -7,13 +8,20 @@ import Menu from "./components/Navbar";
 // going to need a product screen
 // going to need a cartscreen
 
+const client = new ApolloClient({
+  //link for api
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client>
       <Menu />
       <Home />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+v;
