31ba859b7c58ffdb4a61d7226687e8f17e591c2b
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-26 18:19:38 +0000

    clearing out the product cards files

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,13 @@
-import React from "react";
-import Container from "react-bootstrap/esm/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import "./Navbar.css";
 
-function Menu() {
+const Navbar = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+    <nav className="navbar">
+      {/*logo*/}
+      {/*links*/}
+      {/*hamburger menu*/}
+    </nav>
+  )
 }
 
-export default Menu;
+export default Navbar 
