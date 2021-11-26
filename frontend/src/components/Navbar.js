4b71e15e5b5f2d65703f7d19adf88629e1b4bd44
Dylson Oliveira <dylsonuni@gmail.com>
2021-11-26 18:36:14 +0000

    styling the navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,20 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       {/*logo*/}
+        <div className="navbar_logo"> 
+          <h2>MERN shopping cart</h2>
+        </div> 
+
+
       {/*links*/}
+      <ul className="navbar_links"> 
+        <li>
+          <Link>
+          
+          </Link>
+        </li>
+      </ul>
+
       {/*hamburger menu*/}
     </nav>
   )
